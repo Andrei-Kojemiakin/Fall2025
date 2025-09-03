080e5ce9f60164f3356d49170ac795e24d1f1b5d
Andrei-Kojemiakin <kav231@yandex.ru>
2025-09-03 12:56:21 +0300

    HW3.1

---
+++ H_W/H_W_1/HW_1/src/HW_3_1.java
@@ -0,0 +1,18 @@
+public class HW_3_1 {
+    public static void main(String[] args) {
+
+        //Необходимо создать две целочисленные переменные (a, b), присвоить произвольные значения переменным на ваш выбор и вывести следующие строки:
+        //maybe a and b are even - если сумма переменных четная
+        //some variable is odd - если сумма переменных нечетная
+
+        int a =4;
+        int b =4;
+
+        if (( a + b ) % 2 == 0 ) {
+            System.out.println("maybe a and b are even");
+        }else {
+            System.out.println("some variable is odd");
+        }
+
+    }
+}
