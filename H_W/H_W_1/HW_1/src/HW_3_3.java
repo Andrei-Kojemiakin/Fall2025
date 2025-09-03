ffb1b561a40c026a14da14e653236b40a5009881
Andrei-Kojemiakin <kav231@yandex.ru>
2025-09-03 17:05:57 +0300

    HW3.1

---
+++ H_W/H_W_1/HW_1/src/HW_3_3.java
@@ -0,0 +1,10 @@
+public class HW_3_3 {
+    public static void main(String[] args) {
+
+       // Необходимо вывести числа от 0 до 15.
+
+        for (int i =0; i<=15; i++) {
+            System.out.println(i);
+        }
+    }
+}
