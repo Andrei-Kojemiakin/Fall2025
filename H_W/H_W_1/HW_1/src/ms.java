e470e5f7f59bcc38cf115a1e559e1213f72811e0
Andrei-Kojemiakin <kav231@yandex.ru>
2025-09-04 16:33:20 +0300

    HW3.1

---
+++ H_W/H_W_1/HW_1/src/ms.java
@@ -0,0 +1,38 @@
+public class ms {
+    public static void main(String[] args) {
+
+
+
+/*
+        for (int i = 1; i < 9; i++){
+            System.out.printf(" вадрат числа %d равен %d \n", i, i * i);
+        }
+*/
+
+
+
+
+
+
+
+
+        int[] array = new int[] { 1, 2, 3, 4, 5 };
+        for (int i : array){
+
+            System.out.println(i + "-------\n");
+        }
+
+
+        int[] array1 = new int[] { 8, 9, 3, 4, 5 };
+        for (int i = 0; i < array1.length; i++){
+            System.out.println(array1[i]+"+\n");
+
+
+        }
+        int[] array6 = new int[] { 1, 2, 3, 4, 5 };
+        for (int i=0; i<array6.length;i++){
+            array6[i] = array6[i] * 2;
+            System.out.println(array6[i]);
+        }
+    }
+}
