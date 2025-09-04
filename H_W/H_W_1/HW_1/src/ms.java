b67d499e08fea3b6c61ab49d5b0b8237cb21b169
Andrei-Kojemiakin <kav231@yandex.ru>
2025-09-04 16:49:59 +0300

    dealing with arrays 2

--- H_W/H_W_1/HW_1/src/ms.java
+++ H_W/H_W_1/HW_1/src/ms.java
@@ -28,10 +28,24 @@ public class ms {
 
 
         }
-        int[] array6 = new int[] { 1, 2, 3, 4, 5 };
+        int[] array6 = new int[] { 11, 22, 23, 42, 52 };
         for (int i=0; i<array6.length;i++){
             array6[i] = array6[i] * 2;
-            System.out.println(array6[i]);
+            System.out.println(array6[i]+"\n");
+        }
+
+        int[][] nums = new int[][]
+                {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                };
+        for (int i = 0; i < nums.length; i++){
+            for(int j=0; j < nums[i].length; j++){
+
+                System.out.printf("%d ", nums[i][j]);
+            }
+            System.out.println();
         }
     }
 }
